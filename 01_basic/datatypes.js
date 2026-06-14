7afa47e727c509276bcc3174d27d79d04baeb0b0
Ishaan012846 <144902865+Ishaan012846@users.noreply.github.com>
2026-06-14 11:33:13 +0000

    comit changes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,20 @@
+console.log(3+3);
+//alert(3+3) will not work as we are using nodejs not any browser unlike we saw in brocode
+
+//different datatypes in javascript
+
+let name=`ishaan`;
+let num=12;
+let isLoggedIn=true
+let accountState
+
+//number => 2 to the power 53
+//bigint
+//string=" "/' '/` `
+// null=> standalone value
+//typeof(null)===> object
+//undefined=>
+    //typeof(undefined)===>undefined
+    //symbol=>unique
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
