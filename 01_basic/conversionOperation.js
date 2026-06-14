ac340d0043d57f14ca0fd780926f192f9cd23374
Ishaan012846 <144902865+Ishaan012846@users.noreply.github.com>
2026-06-14 16:48:49 +0000

    conversion

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,42 @@
+let score= true
+console.log(typeof score)
+console.log(typeof (score))
+
+let num=Number(score);
+console.log(num)
+console.log(typeof num)//ERROR OUTPUT is number as well as ishaan
+/*OUTPUT TILL LINE 8
+string
+string
+ishaan
+number
+*/
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+//****************operations**************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); =>2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); =>122 if string 1st then concatenation
+// console.log(1 + 2 + "2"); =>32 if no. 1st arithmatic operation then string concatenated
+
+// console.log( (3 + 4) * 5 % 3);
+ 
+// console.log(+true); => 1
+// console.log(+""); =>0
