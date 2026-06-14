79d5b7e610bea847bd35df56ad13bb276c0536db
Ishaan012846 <144902865+Ishaan012846@users.noreply.github.com>
2026-06-14 19:03:08 +0000

    datatypeSummery

---
+++ 01_basic/datatypes-summery.js
@@ -0,0 +1,25 @@
+// primitive and non primitive
+//primitive 7 types- string,number,boolean,null,undefined,Symbol,BigInt
+//copy of the memory is given not the value by refernece 
+
+
+//dymanic or static?
+
+// non-primitive(reference) Array,Object,functions
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn= false 
+const outsideTemp=null
+
+
+let userEmail;// value undefined
+const anotherId=Symbol('123')
+const id= Symbol("123")
+console.log(id===anotherId) // false (not equal)
+
+const bigNumber = 2355645526546565656n
+
+const heros = ["shaktiman","ironman","superman"] //returns typeof=> 'function'
+console.log(typeof heros);
